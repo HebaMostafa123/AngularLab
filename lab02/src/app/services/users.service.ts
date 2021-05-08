d812ea76d36d7bee0e0aacab58f710ccf2440b13
HebaMostafa123 <hoba.mostafa97@gmail.com>
2021-05-08 18:04:33 +0200

    create build to app

--- lab02/src/app/services/users.service.ts
+++ lab02/src/app/services/users.service.ts
@@ -6,7 +6,7 @@ import { User } from '../models/user';
   providedIn: 'root'
 })
 export class UsersService {
-readonly baseURL:string = "http://localhost:3000/users"
+readonly baseURL:string = "https://angularuserdemo.herokuapp.com/users"
   constructor(private myClient:HttpClient) {}
   isUpdated: boolean = false;
    getUsers()
